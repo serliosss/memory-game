a62cfa34c5a565bf04bba3c75fe4a3586fed0907
serliosss <sergey08092006@gmail.com>
2025-09-23 11:15:46 +0500

    layout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+    card.addEventListener("click", ()=>{
+        card.classList.add("card-show");
+    })
+}
