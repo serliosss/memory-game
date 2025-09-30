3753e0394968dd2f8e159190e15e32b083bfe5e7
Студенческая <@rbiu.local>
2025-09-30 11:12:44 +0500

    updateTwoCards

---
+++ src/data.js
@@ -0,0 +1,59 @@
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+import img5 from './img/5.jpg';
+import img6 from './img/6.jpg';
+
+const images = [
+    {
+        'id':1,
+        'url': img1
+    },
+    {
+        'id':7,
+        'url': img1
+    },
+    {
+        'id':2,
+        'url': img2
+    },
+    {
+        'id':8,
+        'url': img2
+    },
+    {
+        'id':3,
+        'url': img3
+    },
+    {
+        'id':9,
+        'url': img3
+    },
+    {
+        'id':4,
+        'url': img4
+    },
+    {
+        'id':10,
+        'url': img4
+    },
+    {
+        'id':5,
+        'url': img5
+    },
+    {
+        'id':11,
+        'url': img5
+    },
+    {
+        'id':6,
+        'url': img6
+    },
+    {
+        'id':12,
+        'url': img6
+    }
+];
+
+export {images}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
-const cards = document.querySelectorAll(".card");
-for (let card of cards) {
+import { images } from "./data.js";
+
+images.sort(() => Math.random() - 0.5);
+
+let visibleCards = [];
+
+const container = document.querySelector(".container");
+images.forEach((image) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("card" + image.id);
+    
+    const img = document.createElement("img");
+    img.src = image.url;
+    img.alt = "";
+    card.append(img);
+
+    container.append(card); 
+
     card.addEventListener("click", ()=>{
-        card.classList.add("card-show");
+        if(visibleCards.length < 2)
+        {
+            card.classList.toggle("card-show");
+
+            visibleCards.push(image);
+            
+            if (visibleCards.length == 2){
+                setTimeout(()=>{
+                card.classList.toggle("card-show");
+                document.querySelector(".card"+visibleCards[0].id).classList.remove("card-show");
+                visibleCards=[];
+                }, 3000)  
+            }
+        }
     })
-}
+});
